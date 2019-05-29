30c4aed08882797230d74d3decfb5697a0e0db15
domo <542760098@qq.com>
2019-05-29 13:16:56 +0900

    Add files via upload

---
+++ delete_fx.lua
@@ -0,0 +1,25 @@
+﻿local tr = aegisub.gettext
+
+script_name = tr"Delete FX"
+script_description = tr"Removes all FX lines by range"
+script_author = "domo"
+script_version = "1"
+
+function delete_fx(subs)
+    for i = 1, #subs do
+        if subs[i].class == "dialogue" and subs[i].effect == "fx" then
+			idx_start = i
+			break
+        end
+    end
+    for i = #subs, 1,-1 do
+        if subs[i].class == "dialogue" and subs[i].effect == "fx" then
+			idx_end = i
+			break
+        end
+    end
+	subs.deleterange(idx_start,idx_end)
+end
+
+
+aegisub.register_macro(script_name, script_description, delete_fx)
