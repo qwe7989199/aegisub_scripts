bbce61078a8d80273754b9fa96533d53a38cad76
domo <qwe7989199@gmail.com>
2019-07-22 22:38:13 +0900

    Update text_stat.lua
    Same as chi ver

--- text_stat.lua
+++ text_stat.lua
@@ -28,6 +28,7 @@ function text_stat(subtitles, selected_lines, active_line)
 	MinLineLength=99999
 	MinLengthIndex=0
 	fullWidthSpaceNum=0
+	TotalDuration=0
 	LineLength={}
 	LineDuration={}
 	min_k={dur=99999,line_index=0}
@@ -71,6 +72,7 @@ function text_stat(subtitles, selected_lines, active_line)
 			LineLength[i]=unicode.len(text_stripped)
 			--Line duration
 			LineDuration[i]=(l.end_time-l.start_time)/1000
+			TotalDuration=TotalDuration+LineDuration[i]
 		end
 	end
 	TotalWordsNum=EngWordsNum+NonEngCharsNum-fullWidthSpaceNum
@@ -112,7 +114,7 @@ function text_stat(subtitles, selected_lines, active_line)
 end
 
 function show_result()
-	if not karaoke or min_k.dur==0 or min_k.dur==0 then 
+	if not karaoke or min_k.dur==3600000 or max_k.dur==0 then 
 		max_k.dur='Not Valid'
 		min_k.dur='Not Valid'
 		max_k.line_index='Not Valid'
@@ -125,12 +127,13 @@ function show_result()
 	config = {
     {x=0, y=0, class="label", label="Results: "},
 	{x=0, y=1, class="label", label="   Selected Line Num: "..TotalLineNum},
-	{x=0, y=2, class="label", label="   Total Syl Num: "..TotalKNum},
-	{x=0, y=3, class="label", label="   Total Words(Space counted): "..TotalWordsNum},
-	{x=0, y=4, class="label", label="   English Words Num: "..EngWordsNum},
-	{x=0, y=5, class="label", label="   Non-English Char Num: "..NonEngCharsNum},
-	{x=0, y=6, class="label", label="   Half-Width Space Num: "..SpaceNum-fullWidthSpaceNum},
-	{x=0, y=7, class="label", label="   Full-Width Space Num: "..fullWidthSpaceNum},
+	{x=0, y=2, class="label", label="   Total Duration: "..TotalDuration},
+	{x=0, y=3, class="label", label="   Total Syl Num: "..TotalKNum},
+	{x=0, y=4, class="label", label="   Total Words(Space counted): "..TotalWordsNum},
+	{x=0, y=5, class="label", label="   English Words Num: "..EngWordsNum},
+	{x=0, y=6, class="label", label="   Non-English Char Num: "..NonEngCharsNum},
+	{x=0, y=7, class="label", label="   Half-Width Space Num: "..SpaceNum-fullWidthSpaceNum},
+	{x=0, y=8, class="label", label="   Full-Width Space Num: "..fullWidthSpaceNum},
 
 	{x=6, y=1, class="label", label="   Max Line Length: "..MaxLineLength.."         | Index: "..MaxLengthIndex},
 	{x=6, y=2, class="label", label="   Min Line Length: "..MinLineLength.."         | Index: "..MinLengthIndex},
@@ -142,23 +145,24 @@ function show_result()
 	btn, result = aegisub.dialog.display(config,{"OK","Save"})
 	if btn=="Save" then
 		scriptname=string.sub(aegisub.file_name(),1,-5)
-		file_name=aegisub.dialog.save("Save to",aegisub.decode_path("?script").."\\", scriptname.."_stats", "*.txt")
+		file_name=aegisub.dialog.save("Save to",aegisub.decode_path("?script").."\\", scriptname.."_stats", "*.csv")
 		if not file_name then aegisub.cancel() end
 		file=io.open(file_name,"w")
-		file:write("Results: \n"
-		.."　　Selected Line Num: "..TotalLineNum.."\n"
-		.."　　Total Syl Num: "..TotalKNum.."\n"
-		.."　　Total Words(Space counted): "..TotalWordsNum.."\n"
-		.."　　English Words Num: "..EngWordsNum.."\n"
-		.."　　Non-English Char Num: "..NonEngCharsNum.."\n"
-		.."　　Half-Width Space Num: "..SpaceNum-fullWidthSpaceNum.."\n"
-		.."　　Full-Width Space Num: "..fullWidthSpaceNum.."\n"
-		.."　　Max Line Length: "..MaxLineLength.."          | Index: "..MaxLengthIndex.."\n"
-		.."　　Min Line Length: "..MinLineLength.."          | Index: "..MinLengthIndex.."\n"
-		.."　　Max Line Duration: "..MaxLineDuration.." (s)  | Index: "..MaxDurationIndex.."\n"
-		.."　　Min Line Duration: "..MinLineDuration.." (s)  | Index: "..MinDurationIndex.."\n"
-		.."　　Max Syl Duration: "..max_k.dur.." (ms)        | Index: "..max_k.line_index.."\n"
-		.."　　Min Syl Duration: "..min_k.dur.." (ms)        | Index: "..min_k.line_index.."\n"
+		file:write("\239\187\191"
+		.."Selected Line Num,"..TotalLineNum.."\n"
+		.."Total Duration,"..TotalDuration.."\n"
+		.."Total Syl Num,"..TotalKNum.."\n"
+		.."Total Words(Space counted),"..TotalWordsNum.."\n"
+		.."English Words Num,"..EngWordsNum.."\n"
+		.."Non-English Char Num,"..NonEngCharsNum.."\n"
+		.."Half-Width Space Num,"..SpaceNum-fullWidthSpaceNum.."\n"
+		.."Full-Width Space Num,"..fullWidthSpaceNum.."\n"
+		.."Max Line Length,"..MaxLineLength..",Index,"..MaxLengthIndex.."\n"
+		.."Min Line Length,"..MinLineLength..",Index,"..MinLengthIndex.."\n"
+		.."Max Line Duration,"..MaxLineDuration..",Index,"..MaxDurationIndex.."\n"
+		.."Min Line Duration,"..MinLineDuration..",Index,"..MinDurationIndex.."\n"
+		.."Max Syl Duration,"..max_k.dur..",Index,"..max_k.line_index.."\n"
+		.."Min Syl Duration,"..min_k.dur..",Index,"..min_k.line_index.."\n"
 		)
 		file:close()
 	end
