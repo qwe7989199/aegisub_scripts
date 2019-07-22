c96254d572b3e815aaeaf99a51f073b673007de5
domo <qwe7989199@gmail.com>
2019-07-22 22:27:07 +0900

    Update text_stat_chi.lua
    1.将导出格式设置为CSV+BOM兼容MS Office
    2.增加统计所选行总持续时间

--- text_stat_chi.lua
+++ text_stat_chi.lua
@@ -28,6 +28,7 @@ function text_stat(subtitles, selected_lines, active_line)
 	MinLineLength=36000000
 	MinLengthIndex=0
 	fullWidthSpaceNum=0
+	TotalDuration=0
 	LineLength={}
 	LineDuration={}
 	min_k={dur=36000000,line_index=0}
@@ -67,10 +68,11 @@ function text_stat(subtitles, selected_lines, active_line)
 			for utf8char in string.gmatch(text_stripped,"[%z\194-\244][\128-\191]*") do
 				NonEngCharsNum=NonEngCharsNum+1
 			end
-			--Line length
+			--Max Line length
 			LineLength[i]=unicode.len(text_stripped)
-			--Line duration
+			--Max Line duration
 			LineDuration[i]=(l.end_time-l.start_time)/1000
+			TotalDuration=TotalDuration+LineDuration[i]
 		end
 	end
 	TotalWordsNum=EngWordsNum+NonEngCharsNum-fullWidthSpaceNum
@@ -112,7 +114,7 @@ function text_stat(subtitles, selected_lines, active_line)
 end
 
 function show_result()
-	if not karaoke or min_k.dur==0 or min_k.dur==0 then 
+	if not karaoke or min_k.dur==3600000 or max_k.dur==0 then 		
 		max_k.dur='不可用'
 		min_k.dur='不可用'
 		max_k.line_index='不可用'
@@ -125,12 +127,13 @@ function show_result()
 	config = {
     {x=0, y=0, class="label", label="统计结果: "},
 	{x=0, y=1, class="label", label="　　已选择的行数: "..TotalLineNum},
-	{x=0, y=2, class="label", label="　　总音节数: "..TotalKNum},
-	{x=0, y=3, class="label", label="　　总字数(含空格): "..TotalWordsNum},
-	{x=0, y=4, class="label", label="　　英文字数: "..EngWordsNum},
-	{x=0, y=5, class="label", label="　　非英文字数: "..NonEngCharsNum},
-	{x=0, y=6, class="label", label="　　半角空格数: "..SpaceNum-fullWidthSpaceNum},
-	{x=0, y=7, class="label", label="　　全角空格数: "..fullWidthSpaceNum},
+	{x=0, y=2, class="label", label="　　总持续时间: "..TotalDuration},
+	{x=0, y=3, class="label", label="　　总音节数: "..TotalKNum},
+	{x=0, y=4, class="label", label="　　总字数(含空格): "..TotalWordsNum},
+	{x=0, y=5, class="label", label="　　英文字数: "..EngWordsNum},
+	{x=0, y=6, class="label", label="　　非英文字数: "..NonEngCharsNum},
+	{x=0, y=7, class="label", label="　　半角空格数: "..SpaceNum-fullWidthSpaceNum},
+	{x=0, y=8, class="label", label="　　全角空格数: "..fullWidthSpaceNum},
 
 	{x=1, y=1, width=15, class="label", label="　　字符最多的行: "..MaxLineLength.."        | 行号: "..MaxLengthIndex},
 	{x=1, y=2, width=15, class="label", label="　　字符少的行: "..MinLineLength.."          | 行号: "..MinLengthIndex},
@@ -142,23 +145,24 @@ function show_result()
 	btn, result = aegisub.dialog.display(config,{"OK","Save"})
 	if btn=="Save" then
 		scriptname=string.sub(aegisub.file_name(),1,-5)
-		file_name=aegisub.dialog.save("保存文件到",aegisub.decode_path("?script").."\\", scriptname.."_stats", "*.txt")
+		file_name=aegisub.dialog.save("保存文件到",aegisub.decode_path("?script").."\\", scriptname.."_stats", "*.csv")
 		if not file_name then aegisub.cancel() end
 		file=io.open(file_name,"w")
-		file:write("统计结果: \n"
-		.."　　已选择的行数: "..TotalLineNum.."\n"
-		.."　　总音节数: "..TotalKNum.."\n"
-		.."　　总字数(含空格): "..TotalWordsNum.."\n"
-		.."　　英文字数: "..EngWordsNum.."\n"
-		.."　　非英文字数: "..NonEngCharsNum.."\n"
-		.."　　半角空格数: "..SpaceNum-fullWidthSpaceNum.."\n"
-		.."　　全角空格数: "..fullWidthSpaceNum.."\n"
-		.."　　字符最多的行: "..MaxLineLength.."        | 行号: "..MaxLengthIndex.."\n"
-		.."　　字符少的行: "..MinLineLength.."          | 行号: "..MinLengthIndex.."\n"
-		.."　　时间最长的行: "..MaxLineDuration.." (秒) | 行号: "..MaxDurationIndex.."\n"
-		.."　　时间最短的行: "..MinLineDuration.." (秒) | 行号: "..MinDurationIndex.."\n"
-		.."　　时间最长的音节: "..max_k.dur.." (毫秒)   | 行号: "..max_k.line_index.."\n"
-		.."　　时间最短的音节: "..min_k.dur.." (毫秒)   | 行号: "..min_k.line_index.."\n"
+		file:write("\239\187\191"
+		.."已选择的行数,"..TotalLineNum.."\n"
+		.."总持续时间,"..TotalDuration.."\n"
+		.."总音节数,"..TotalKNum.."\n"
+		.."总字数(含空格),"..TotalWordsNum.."\n"
+		.."英文字数,"..EngWordsNum.."\n"
+		.."非英文字数,"..NonEngCharsNum.."\n"
+		.."半角空格数,"..SpaceNum-fullWidthSpaceNum.."\n"
+		.."全角空格数,"..fullWidthSpaceNum.."\n"
+		.."字符最多的行,"..MaxLineLength..",行号,"..MaxLengthIndex.."\n"
+		.."字符少的行,"..MinLineLength..",行号,"..MinLengthIndex.."\n"
+		.."时间最长的行,"..MaxLineDuration..",行号,"..MaxDurationIndex.."\n"
+		.."时间最短的行,"..MinLineDuration..",行号,"..MinDurationIndex.."\n"
+		.."时间最长的音节,"..max_k.dur..",行号,"..max_k.line_index.."\n"
+		.."时间最短的音节,"..min_k.dur..",行号,"..min_k.line_index.."\n"
 		)
 		file:close()
 	end
