a3c458bfdf9e0a43b1778c6bdb5d9b65fb08e9d1
domo <qwe7989199@gmail.com>
2020-08-07 20:30:06 +0900

    change to lfs, optimize request size

--- CNSpellChecker.lua
+++ CNSpellChecker.lua
@@ -2,20 +2,20 @@ local tr = aegisub.gettext
 script_name = tr"文本纠错"
 script_description = tr"利用百度的API进行文本纠错"
 script_author = "domo"
-script_version = "0.1"
+script_version = "0.2"
 
 local request = require('luajit-request')
 local json = require('json')
-local utf8 = require('utf8')
+-- local utf8 = require('utf8')
 local ffi = require"ffi"
 local C = ffi.C
-
+local lfs = require('lfs')
 ffi.cdef[[
 void Sleep(int ms);
 ]]
 
 -- Necessary
-local AccessToken = ""
+local AccessToken = 
 
 function split(str, split_char)
 	local sub_str_tab = {}
@@ -50,7 +50,7 @@ end
 
 function showSuggestion(orgText,startLineNum)
 	-- aegisub.debug.out(utf8.len(orgText))
-	-- aegisub.debug.out(string.len(orgText))
+	-- aegisub.debug.out(tostring(string.len(orgText)).."长度\n")
 	orgTextTbl = split(orgText,"&")
 	response = sendRequest(orgText)
 	result = json.decode(response)
@@ -61,13 +61,13 @@ function showSuggestion(orgText,startLineNum)
 		resultWords = result.item["vec_fragment"]
 		lineCount = 1
 		allNewWords = {}
-		andPos = string.find(wholeResult,"&") or string.len(wholeResult)
+		andPos = string.find(wholeResult,"&") or string.len(wholeResult) --得到 & 的位置，结合beginPos可以知道错别字归属于哪行
 		for i=1,#resultWords do
 			wrongWord = resultWords[i]["ori_frag"]
 			guessWord = resultWords[i]["correct_frag"]
 			beginPos = resultWords[i]["begin_pos"]
 			endPos = resultWords[i]["end_pos"]
-			if beginPos > andPos-1 then
+			while beginPos > andPos-1 do --如果错别字的beginPos 比 & 的位置大，那么就进入下一行
 				lineCount = lineCount + 1
 				andPos = string.find(wholeResult,"&",andPos+1)
 			end
@@ -81,7 +81,7 @@ function showSuggestion(orgText,startLineNum)
 				content = "---------------第"..tostring(startLineNum+i-#resultTextTbl).."行---------------\n建议："..orgTextTbl[i].."->"..resultTextTbl[i]..'\n细节：\n'
 				aegisub.debug.out(content)
 				file:write(content)
-				for j=#allNewWords,1,-1 do
+				for j=#allNewWords,1,-1 do --涉及
 					if allNewWords[j].inLine==i then
 						aegisub.debug.out(allNewWords[j].wrongWord..'->'..allNewWords[j].guessWord..'\n')
 						file:write(allNewWords[j].wrongWord..'->'..allNewWords[j].guessWord..'\n')
@@ -95,12 +95,16 @@ end
 
 function checkText(subtitles,selected_lines)
 	local requestNum = 0
-	local path = aegisub.decode_path("?data".."\\CNCheckHis")
+	local path = aegisub.decode_path("?data")
+	lfs.chdir(path)
 	local timestamp = os.date("%Y%m%d_%H%M%S")
-	fileName = path.."\\"..timestamp..".txt"
-	if not io.open(path, "rb") then
-		os.execute("mkdir "..path)
+	--检查是否存在历史存储目录
+	tmpfile,err = io.open(path.."\\CNCheckHist")
+	if string.find(err,"No such") then
+		aegisub.debug.out("尝试创建目录")
+		lfs.mkdir("CNCheckHist") --用lfs建文件夹，os.execute会有命令行弹出来，不优雅
 	end
+	fileName = path.."\\CNCheckHist".."\\"..timestamp..".txt"
 	file = io.open(fileName,"w")
 	for i=1,#subtitles do
 		if subtitles[i].class=="dialogue" then
@@ -114,28 +118,29 @@ function checkText(subtitles,selected_lines)
 		if string.len(l.text)>=511 then
 			aegisub.debug.out("第"..tostring(i-dialogue_start+1).."行的文本过长，无法处理。")
 		end
-		if (string.len(orgText)+string.len(l.text.."&"))<511 and z~=#selected_lines then
-			orgText = orgText..l.text.."&"
-			multipleLine = true
-		else
-			if multipleLine then
-				showSuggestion(orgText,i-dialogue_start+1)
-				requestNum = requestNum + 1
-				C.Sleep(250)
-			end
-			multipleLine = false
-			orgText = l.text
-			showSuggestion(orgText,i-dialogue_start+1)
-			requestNum = requestNum + 1
+		--在行与行之间插入&作为分割符号
+		if (string.len(orgText)+string.len(l.text.."&"))<511 and z~=#selected_lines then 
+			orgText = orgText..l.text.."&" 					--如果长度没有超过511，可以合并文本
+		elseif (string.len(orgText)+string.len(l.text.."&"))>=511 and z~=#selected_lines then
+			-- aegisub.debug.out(tostring(string.len(orgText)).."多行长度\n")
+			showSuggestion(orgText,i-dialogue_start+1) --请求api给出建议
 			C.Sleep(250)
+			orgText = ""
+			requestNum = requestNum + 1					--请求计数+1
+		else
+			showSuggestion(l.text,i-dialogue_start+1) --请求api给出建议
+			C.Sleep(250)
+			requestNum = requestNum + 1					--请求计数+1
 		end
+		aegisub.progress.set(z/#selected_lines*100)
+		aegisub.progress.task("已处理到第"..tostring(selected_lines[z]-dialogue_start+1).."行")
 	end
+	aegisub.debug.out("完成，共请求"..tostring(requestNum).."次")
 	file:close()
 	btn, result = aegisub.dialog.display({{class="label",label="是否保留本地文件？"}},{"OK","Cancel"})
 	if btn ~= "OK" then
 		os.remove(fileName)
 	end
-	aegisub.debug.out("完成，共请求"..tostring(requestNum).."次")
 end
 
 aegisub.register_macro(script_name, script_description, checkText)
